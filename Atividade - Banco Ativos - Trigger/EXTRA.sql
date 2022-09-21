4c8c110c71553309e511a5a7648cb9ca5fc82613
Fabrício <fabricio.londero@ufn.edu.br>
2022-09-21 08:09:00 -0300

    Create EXTRA.sql

---
+++ Atividade - Banco Ativos - Trigger/EXTRA.sql
@@ -0,0 +1,6 @@
+
+--EXTRA - Trigger
+
+--Um sistema possui uma tabela para armazenar os registros dos PRODUTOS que comercializa, e tambem uma tabela de REQUISICOES que pode ser de entrada ou de saída, além da MOVIMENTACAO, que pode ser de entrada ou de saida - uma requisição que foi confirmada.
+
+--Faça uma trigger que gere uma nova REQUISICAO de entrada caso as requisições de saída atinjam mais de 70% da quantidade do item em questão em estoque.
