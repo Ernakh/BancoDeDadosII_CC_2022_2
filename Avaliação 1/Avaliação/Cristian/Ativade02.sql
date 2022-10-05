14b80d01838f3be61d5a3f3afc308c127887d116
Cristian R. Campos <94974574+CristianRCampos@users.noreply.github.com>
2022-10-05 08:11:44 -0300

    Add files via upload

---
+++ Avaliação 1/Avaliação/Cristian/Ativade02.sql
@@ -0,0 +1,49 @@
+﻿create database loja 
+
+create table venda
+(
+	id_venda integer primary key identity,
+	quantidade int
+
+)
+
+create table estoque
+(
+	id_estoque integer primary key identity,
+	fk_venda integer,
+	estoque_atual int ,
+	foreign key (fk_venda) references venda(id_venda)
+)
+
+select * from venda
+select * from estoque
+
+insert into venda (quantidade)
+values (20)
+
+insert into estoque (estoque_atual)
+values (100)
+
+
+CREATE trigger calcula_estoque
+ON venda
+AFTER INSERT
+AS
+BEGIN
+	DECLARE 
+	@valor_venda integer,
+	@id_mod integer,
+	@print_estoque integer
+
+	SELECT @valor_venda = quantidade, @id_mod = fk_id_estoque FROM INSERTED 
+
+	UPDATE estoque 
+	SET estoque_atual = (estoque_atual - @valor_venda) 
+	WHERE id_estoque = @id_mod
+	
+	SELECT @print_estoque = estoque_atual FROM estoque WHERE id_estoque = @id_mod 
+
+	PRINT 'Estoque do produto após operação: ' + CAST(@print_estoque AS NVARCHAR(100)) 
+
+END
+GO
---
+++ Avaliação 1/Avaliação/Cristian/Atividade03.sql
@@ -0,0 +1,41 @@
+﻿create database ex03 
+
+create table pessoa
+(
+	id_pessoa integer not null primary key identity,
+	nome varchar(45),
+	sexo varchar(20)
+)
+
+
+select * from pessoa 
+
+insert into pessoa (nome, sexo)
+values ('João', 'masculino')
+
+insert into pessoa (nome, sexo)
+values ('Maria', 'feminino')
+
+create trigger triggerinsert
+on pessoa
+after insert
+as
+	
+	 declare @sexo integer
+     declare @masculino integer
+
+     select @sexo = (select inserted.sexo from inserted)
+    
+	
+	IF @sexo = @masculino
+		begin
+			insert into pessoa values ('Inserção', ' Sr. (+ @nome + )');
+		end
+
+	 ELSE
+		begin
+		insert into pessoa values ('Inserção', ' Sra. (+ @nome + )');
+		end
+end
+
+select * from pessoa 
