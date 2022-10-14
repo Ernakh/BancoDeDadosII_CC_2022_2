77a9260c02c63870bc65c048961d669246a8315d
Fabrício <fabricio.londero@ufn.edu.br>
2022-10-14 08:17:40 -0300

    Create Exercício.sql

---
+++ Aula Restrições de Integridade/Exercício.sql
@@ -0,0 +1,16 @@
+
+
+Crie um banco de dados para gerenciar cursos e suas matrículas.
+O banco precisa armazenar: professores, alunos, cursos, disciplinas, curriculo (vincula curso à disciplina) e turma (vincula professor a disciplina)
+
+Para professor:
+- o nome é um campo obrigatório;
+- o RG é um atributo que tem valor único para cada professor
+- o sexo pode ser: 'M' ou 'F'
+- o idade deve estar entre 21 e 70 anos
+- o titulação deve ser: 'especialista', 'mestre', 'doutor' pu 'pós-doutor'
+- o categoria deve ser: 'auxiliar', 'assistente', 'adjunto' ou 'titular'
+
+
+
+
