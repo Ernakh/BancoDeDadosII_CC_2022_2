d842585aad77987145c024eb047c3edc99d07d6a
Fabrício <fabricio.londero@ufn.edu.br>
2022-11-23 08:14:25 -0300

    Update scripts.sql

--- Aula - Gerenciamento de Acessos/scripts.sql
+++ Aula - Gerenciamento de Acessos/scripts.sql
@@ -67,3 +67,12 @@ FROM sys.database_role_members roles
 	JOIN sys.database_principals pp ON roles.role_principal_id = pp.principal_id
 ORDER BY 1
 
+
+
+SELECT
+p.name AS [Name] ,r.type_desc,r.create_date , r.modify_date,r.default_database_name
+FROM
+sys.server_principals r
+INNER JOIN sys.server_role_members m ON r.principal_id = m.role_principal_id
+INNER JOIN sys.server_principals p ON
+p.principal_id = m.member_principal_id
